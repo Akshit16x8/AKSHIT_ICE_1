a81a027e4b4c4c3f4d7f67e60b57816d59f740c0
Akshit16x8 <akshit4@sheridancollege.ca>
2025-06-05 21:13:43 -0400

    Update CardTrick.java

--- ICE1/src/main/java/ca/sheridancollege/akshit/ice1/CardTrick.java
+++ ICE1/src/main/java/ca/sheridancollege/akshit/ice1/CardTrick.java
@@ -1,32 +1,48 @@
 package ca.sheridancollege.akshit.ice1;
 
-import ca.sheridancollege.akshit.ice1.Card;
 import java.util.Random;
 
 /**
  * Modifier: Akshit  
  * Student ID: 991764651
  * Date: June 3, 2025
- * 
- * This class generates a hand of 7 random cards as part of ICE 1 Step 4.
  */
 public class CardTrick {
+
     public static void main(String[] args) {
         Card[] magicHand = new Card[7];
         Random rand = new Random();
 
-        // Step 4: Fill magicHand with random cards
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            c.setValue(rand.nextInt(13) + 1); // values from 1 to 13
-            c.setSuit(Card.SUITS[rand.nextInt(4)]); // random suit
+            c.setValue(rand.nextInt(13) + 1);
+            c.setSuit(Card.SUITS[rand.nextInt(4)]);
             magicHand[i] = c;
         }
 
-        // Print the generated magic hand (optional, for verification)
-        System.out.println("Magic Hand (Step 4):");
+        Card luckyCard = new Card();
+        luckyCard.setValue(7);
+        luckyCard.setSuit("Hearts");
+
+        boolean found = false;
         for (Card c : magicHand) {
-            System.out.println(c.getValue() + " of " + c.getSuit());
+            if (c.getValue() == luckyCard.getValue() &&
+                c.getSuit().equalsIgnoreCase(luckyCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+
+        System.out.println("Lucky Card: " + luckyCard);
+        if (found) {
+            System.out.println("🎉 You win!");
+        } else {
+            System.out.println("❌ Try again.");
+        }
+
+        System.out.println("\nMagic Hand:");
+        for (Card c : magicHand) {
+            System.out.println(c);
         }
     }
 }
