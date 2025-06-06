0a099dc3432ea165fb93b3589d958b082fb2184a
Akshit <akshit4@sheridancollege.ca>
2025-06-05 20:58:31 -0400

    Created Project

---
+++ ICE1/src/main/java/ca/sheridancollege/akshit/ice1/ICE1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ca.sheridancollege.akshit.ice1;
+
+/**
+ *
+ * @author akshi
+ */
+public class ICE1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
